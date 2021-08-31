f4bdf479950baa8ebcd3d9d1249c6fb95ebb2783
hayoung <hy3639@gmail.com>
2021-08-31 15:51:12 +0900

    작업중

--- pc/js/common.js
+++ pc/js/common.js
@@ -62,6 +62,16 @@ $(document).ready(function(){
 		$('.quick-item .depth').removeClass('on').hide();
 	}
 
+	/* 상단 공지 슬라이드 */
+	var swiper = new Swiper(".cont-top .swiper-container", {
+        direction: "vertical",
+		  // Navigation arrows
+		  navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},       
+    });
+
 
 
 
@@ -83,6 +93,52 @@ $(document).ready(function(){
 		$(this).next('.tooltip-layer').hide();
 	});
 
+	/* 테이블 오버시 버튼노출 */
+	$('.layer-box').mouseenter(function(){
+		$(this).find('button').show();
+	});
+	$('.layer-box').mouseleave(function(){
+		$(this).find('button').hide();
+	});
+
+
+	/* datepicker */
+	$('.cal').each(function(){
+		$(this).find('input').datepicker({
+		   dateFormat: "yy-mm-dd", 
+		  // showMonthAfterYear: true ,
+		  // monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월']
+
+		  showOn: "both", // 버튼과 텍스트 필드 모두 캘린더를 보여준다. 
+		  buttonImage: "../images/icon/icon_20_date.png", 
+		  buttonImageOnly: true, // 버튼에 있는 이미지만 표시한다. 
+		  changeMonth: false, // 월을 바꿀수 있는 셀렉트 박스를 표시한다. 
+		  changeYear: false, // 년을 바꿀 수 있는 셀렉트 박스를 표시한다. 
+		  minDate: '-100y', // 현재날짜로부터 100년이전까지 년을 표시한다. 
+		  nextText: '다음 달', // next 아이콘의 툴팁.
+		   prevText: '이전 달', // prev 아이콘의 툴팁. 
+		  numberOfMonths:1, // 한번에 얼마나 많은 월을 표시할것인가. [2,3] 일 경우, 2(행) x 3(열) = 6개의 월을 표시한다. 
+		  //stepMonths: 3, // next, prev 버튼을 클릭했을때 얼마나 많은 월을 이동하여 표시하는가. 
+		  //yearRange: 'c-100:c+10', // 년도 선택 셀렉트박스를 현재 년도에서 이전, 이후로 얼마의 범위를 표시할것인가.
+		  // showButtonPanel: true, // 캘린더 하단에 버튼 패널을 표시한다. 
+		  // closeText: '닫기', // 닫기 버튼 패널 
+		  
+		   showMonthAfterYear: true , // 월, 년순의 셀렉트 박스를 년,월 순으로 바꿔준다. 
+		   dayNamesMin: ['SUN','MON','TUE','WED','THU','FRI','SAT'],
+		   monthNames: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+		   yearSuffix: '.',
+		  showOtherMonths:true,
+	  
+		  
+		  
+		  
+		  
+
+
+
+		});
+	});
+	
 	
 
 
