f8446c8636b0d12e4b308a0ced98f76c411d6f89
hayoung <hy3639@gmail.com>
2021-10-15 11:22:41 +0900

    아이디관리 완료

--- pc/js/common.js
+++ pc/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	resQuick(); // 반응형 퀵
+	loding(); //로딩바
 	
 
 /*======= 공통 (레이아웃) ======= */
@@ -447,3 +448,73 @@ function popClose(){
 function openWindowPop(url, name, options){ 
     window.open(url, name, options);
 }
+
+
+/*======= 로딩바 ======= */
+var cSpeed=9;
+var cWidth=64;
+var cHeight=64;
+var cTotalFrames=8;
+var cFrameWidth=64;
+var cImageSrc='../../images/layout/sprites.gif';
+
+var cImageTimeout=false;
+var cIndex=0;
+var cXpos=0;
+var cPreloaderTimeout=false;
+var SECONDS_BETWEEN_FRAMES=0;
+
+function startAnimation(){	
+	document.getElementById('loaderImage').style.backgroundImage='url('+cImageSrc+')';
+	document.getElementById('loaderImage').style.width=cWidth+'px';
+	document.getElementById('loaderImage').style.height=cHeight+'px';
+	
+	//FPS = Math.round(100/(maxSpeed+2-speed));
+	FPS = Math.round(100/cSpeed);
+	SECONDS_BETWEEN_FRAMES = 1 / FPS;
+	
+	cPreloaderTimeout=setTimeout('continueAnimation()', SECONDS_BETWEEN_FRAMES/1000);
+}
+
+
+function continueAnimation(){
+	
+	cXpos += cFrameWidth;
+	//increase the index so we know which frame of our animation we are currently on
+	cIndex += 1;
+		
+	//if our cIndex is higher than our total number of frames, we're at the end and should restart
+	if (cIndex >= cTotalFrames) {
+		cXpos =0;
+		cIndex=0;
+	}
+	
+	if(document.getElementById('loaderImage'))
+		document.getElementById('loaderImage').style.backgroundPosition=(-cXpos)+'px 0';
+	
+	cPreloaderTimeout=setTimeout('continueAnimation()', SECONDS_BETWEEN_FRAMES*1000);
+}
+
+function stopAnimation(){//stops animation
+	clearTimeout(cPreloaderTimeout);
+	cPreloaderTimeout=false;
+}
+
+function imageLoader(s, fun)//Pre-loads the sprites image
+{
+	clearTimeout(cImageTimeout);
+	cImageTimeout=0;
+	genImage = new Image();
+	genImage.onload=function (){cImageTimeout=setTimeout(fun, 0)};
+	genImage.onerror=new Function('alert(\'Could not load the image\')');
+	genImage.src=s;
+}
+
+function loding(){
+	$('.loding').each(function(){
+		//The following code starts the animation
+		new imageLoader(cImageSrc, 'startAnimation()');
+	});
+}
+
+/*=======// 로딩바 ======= */
